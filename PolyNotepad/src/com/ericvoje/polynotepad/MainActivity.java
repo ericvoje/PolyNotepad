829debb4855c99864d95da8d694a94eaf4d3f367
ericvoje <ericvoje@gmail.com>
2014-04-26 01:41:42 -0500

    NoteActivity created, some UI elements initialized

--- PolyNotepad/src/com/ericvoje/polynotepad/MainActivity.java
+++ PolyNotepad/src/com/ericvoje/polynotepad/MainActivity.java
@@ -1,15 +1,24 @@
 package com.ericvoje.polynotepad;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	Button newButton, loadButton, settingsButton;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		newButton = (Button) findViewById(R.id.new_button);
+		loadButton = (Button) findViewById(R.id.load_button);
+		settingsButton = (Button) findViewById(R.id.settings_button);
 	}
 
 	@Override
@@ -17,6 +26,22 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	// Starts NoteActivity with blank note
+	public void newNote(View view) {
+		Intent intent = new Intent(this, NoteActivity.class);
+		intent.putExtra("NEW_NOTE", true);
+		startActivity(intent);
+	}
+
+	// Starts NoteActivity with a saved note
+	public void loadNote(View view) {
+		Intent intent = new Intent(this, NoteActivity.class);
+		intent.putExtra("NEW_NOTE", false);
+		// Add file name
+		intent.putExtra("NOTE_FILE", "loadnote.txt");
+		startActivity(intent);
 	}
 
 }
